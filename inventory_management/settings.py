6b1891f144d92de4787359abb0ba6bf6624ea900
kolianita22 <kolianita784@gmail.com>
2026-01-02 01:50:36 +0530

    Add recurement

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*!9opwber%8159wvz&n4-g!1#rl25=!2@ilm_(m845&#k9#sbo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
